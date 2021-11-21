da174152eb44800985baa900e85b6de36176f964
andres <andresazcurra@gmail.com>
2021-11-21 05:14:33 -0300

    Esto es el trabajo de la unidad 2 de php

---
+++ index.php
@@ -0,0 +1,70 @@
+<?php  
+
+
+
+?>
+
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <style>   
+    * {
+        margin: 0;
+        padding: 0;
+        background-color: lightsteelblue;
+        text-align: center;
+        
+    }
+  
+    li {
+  background-color: powderblue;
+  transition: background-color .5s;
+}
+
+li:hover {
+  background-color: gold;
+}
+         
+         
+         
+         
+         
+         </style>
+
+    <title>trabajo practico unidad 2 back</title>
+  </head>
+  <body>
+    <h1>trabajo practico PHP</h1>
+    <div>
+        <ul>
+
+
+        <li><?php  echo "Nombre : Andres"  ?></li>
+        <li><?php  echo "Apellido : Azcurra"  ?></li>
+        <li><?php  echo "Edad : 38 casi 39"  ?></li>
+        <li><?php  echo "Editor de codigo : visual studio code"  ?></li>
+        <li><?php  echo "Sistema Operativi : windows 10"  ?></li>
+        <li><?php  echo "codear, comer,  cocinar.  "  ?></li>
+
+        </ul>
+    </div>
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+    -->
+  </body>
+</html>
